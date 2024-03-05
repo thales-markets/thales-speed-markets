4f88ad8fe40d09e09daff5cb5bd03c9aaf05cfa4
Gorshtak <sg31191@gmail.com>
2024-03-05 12:40:14 +0100

    added redux

---
+++ src/redux/rootReducer.ts
@@ -0,0 +1,12 @@
+import { combineReducers } from "@reduxjs/toolkit";
+import app from "./modules/app";
+import ui from "./modules/ui";
+import wallet from "./modules/wallet";
+
+const rootReducer = combineReducers({
+  app,
+  wallet,
+  ui,
+});
+
+export default rootReducer;
