cb5cfa9f508525e3e15d54e6f8532434fb39f18c
Gorshtak <sg31191@gmail.com>
2024-03-06 14:01:51 +0100

    removed MUI leftovers

--- src/components/SimpleLoader/SimpleLoader.tsx
+++ src/components/SimpleLoader/SimpleLoader.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import CircularProgress from '@material-ui/core/CircularProgress';
 import styled from 'styled-components';
 
 const SimpleLoader: React.FC = () => {
     return <StyledLoader />;
 };
 
-const StyledLoader = styled(CircularProgress)`
+const StyledLoader = styled.div`
     position: absolute;
     left: calc(50% - 22px);
     top: calc(50% - 22px);
