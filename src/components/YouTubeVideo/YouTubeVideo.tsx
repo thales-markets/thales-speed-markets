954120d8b1beedcced17301053e764422f2e6c24
Sasa Lotina <lotinasa@gmail.com>
2024-07-03 16:20:36 +0200

    Add speed youtube video

--- src/components/YouTubeVideo/YouTubeVideo.tsx
+++ src/components/YouTubeVideo/YouTubeVideo.tsx
@@ -19,6 +19,7 @@ const YouTubeVideo: React.FC<YouTubeVideoProps> = ({ source, title }) => {
 
 const IFrame = styled.iframe`
     width: 100%;
+    height: 100%;
     aspect-ratio: 16 / 9;
     border-radius: inherit;
 `;
